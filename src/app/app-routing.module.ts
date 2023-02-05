22e63cefd115c81db96ac88bc53fdc8868a88ade
Kody Spring <36212983+kodyspring@users.noreply.github.com>
2023-02-05 15:54:40 -0500

    layout updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: '', component: AppComponent }, //LOAD BY COMPONENT
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) } //LAZY LOADING
 ];
 
 @NgModule({
